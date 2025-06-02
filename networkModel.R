9226752c4f7b6d571e050ef697715a757785b3b7
Jane Doe <drewung@uw.edu>
2025-06-02 13:33:46 -0700

    package draft

---
+++ networkModel.R
@@ -0,0 +1,26 @@
+---
+  title:
+  output: 
+  authors
+---
+  
+
+  
+```{r}
+#run devtools to download first
+install.packages("devtools")
+install.packages("Rtools")
+
+#apply pacakge
+library(devtools)
+library(Rtools)
+
+#then run this code
+#setup code
+
+#run this in console,
+#devtools::install_github("abresler/nbastatR")
+
+#finailzie nbastatR
+library(nbastatR)
+```
